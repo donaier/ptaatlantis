e75855adf5255bb7f4246dd7f734236161eface0
Donat Baier <donat.baier@gmail.com>
2014-05-16 19:39:40 +0200

    settings + layout

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,6 +1,7 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     navigation.selected_class = 'active'
+    primary.dom_class = 'side-nav'
     Kuhsaft::Page.find_by_slug_de('home').children.published.each do |page|
       unless page.url.blank?
         primary.item page.id, page.title, "/#{page.url_without_locale}" do |sub_nav|
