d8f4c913cda8831c1af37e3c9e24008b573d5b5f
Donat Baier <donat.baier@gmail.com>
2014-05-23 12:02:30 +0200

    paperclip reset

--- app/models/kuhsaft/image_brick.rb
+++ app/models/kuhsaft/image_brick.rb
@@ -2,7 +2,9 @@ module Kuhsaft
   class ImageBrick < Brick
 
     has_attached_file :image,
-      styles: { big: '1000x800>', thumb: '120x100>' }
+      styles: { big: '1000x800>', thumb: '120x100>' },
+      :path => ":rails_root/public/system/:class/:attachment/:id/:style/:filename",
+      :url => "/system/:class/:attachment/:id/:style/:filename"
 
     validates :image, :presence => true
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
--- config/initializers/paperclip.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.production?
-  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-end
-
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
