518eb5c9edd6c85694acc158b0c81ed0a34e5754
Donat Baier <donat.baier@gmail.com>
2015-10-18 15:06:10 +0200

    add new mappins to meeting point selection

--- app/models/mappin.rb
+++ app/models/mappin.rb
@@ -11,6 +11,14 @@ class Mappin < ActiveRecord::Base
 
   index_attributes :lat, :lng, :marker_text
 
+  def self.pin_array
+    pin_array = []
+    self.all.each do |p|
+      pin_array << p.marker_text
+    end
+    pin_array
+  end
+
   def touch_map_bricks
     Kuhsaft::Brick.where(type: 'Kuhsaft::MapBrick').each do |map_brick|
       map_brick.touch
